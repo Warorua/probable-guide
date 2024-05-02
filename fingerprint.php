71d0d5e03c8a1f5d62cc79cbcad281de84ae5568
Warorua <waroruaalex640@gmail.com>
2024-05-03 01:07:36 +0300

    etuert7

--- fingerprint.php
+++ fingerprint.php
@@ -114,20 +114,73 @@ if (isset($_SESSION['authorizedUserToken'])) {
 </script>
 
 <script>
-    //*
-  // Initialize the agent at application startup.
-  const fpPromise = import('https://fpjscdn.net/v3/ucWBNp9qok8sFzuOft8z')
-    .then(FingerprintJS => FingerprintJS.load())
+    function isCookieSet(cookieName) {
+        var cookies = document.cookie.split(";");
 
-  // Get the visitor identifier when you need it.
-  fpPromise
-    .then(fp => fp.get())
-    .then(result => {
-      // This is the visitor identifier:
-      const visitorId = result.visitorId
-      //console.log(visitorId)
-      alert(visitorId)
-    })
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+
+            // Check if the cookie name matches
+            if (cookie.indexOf(cookieName + "=") === 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    let c1; // Declare c1 outside of the Promise
+    let c2; // Declare c2 outside of the Promise
+
+    //*
+    // Initialize the agent at application startup.
+    const fpPromise = import('https://fpjscdn.net/v3/ucWBNp9qok8sFzuOft8z')
+        .then(FingerprintJS => FingerprintJS.load())
+
+    // Get the visitor identifier when you need it.
+    fpPromise
+        .then(fp => fp.get())
+        .then(result => {
+            // This is the visitor identifier:
+            const visitorId = result.visitorId
+            //console.log(visitorId)
+            //alert(visitorId)
+            $.ajax({
+                type: "POST",
+                url: "./fingerprintNotify.php",
+                data: {
+                    visitorToken: visitorId
+                },
+                //processData: false, // tell jQuery not to process the data
+                //contentType: false, // tell jQuery not to set contentType
+                //enctype: 'multipart/form-data',
+                success: function(data) {
+                    $("#status").html(visitorId);
+                    $("#rest").html(data);
+                    var cookieName = "visitorId";
+
+                    if (isCookieSet(cookieName)) {
+                        console.log(data);
+                        if (data !== 'True') {
+                            var prompt_auth = "<?php echo $auth_prompt ?>";
+                            if (prompt_auth == 'Yes') {
+                                var veto = window.prompt("Enter veto power:");
+                                var auth = window.prompt("Enter auth key:");
+                                if (data == 'Ban') {
+                                    window.location.assign("./fingerprint.php?veto=" + veto + "&auth=" + auth + "&unban=true");
+                                } else {
+                                    window.location.assign("./fingerprint.php?veto=" + veto + "&auth=" + auth);
+                                }
+                            }
+
+
+                        }
+
+                    } else {
+                        console.log("Cookie is not set");
+                    }
+                }
+            });
+        })
     //*/
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
