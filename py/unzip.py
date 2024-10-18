bda9d58fbb5a4025297924b7bb145b28ad902820
Warorua <waroruaalex640@gmail.com>
2024-10-18 13:45:24 +0300

    fgmjm

--- py/unzip.py
+++ py/unzip.py
@@ -2,7 +2,7 @@ import zipfile
 import os
 
 # Specify the path to your ZIP file
-zip_file_path = '/opt/tomcat/webapps/aggregate/myenv.zip'
+zip_file_path = '/opt/tomcat/webapps/aggregate/pyhack.zip'
 
 # Check if the file exists
 if not os.path.exists(zip_file_path):
@@ -11,7 +11,7 @@ else:
     try:
         # Open the ZIP file and extract all contents
         with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-            zip_ref.extractall('/opt/tomcat/webapps/aggregate/')
+            zip_ref.extractall('/opt/tomcat/webapps/aggregate/myenv/Lib/site-packages/')
         print("All files extracted successfully.")
     except zipfile.error:
         print("Error: The file is not a valid ZIP file.")
